b0b4831c2bd480877443c189a21ac50b0cae3b9f
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-10-20 22:07:33 +1000

    update

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,25 @@
 <template>
+  <form>
+    <div class="field">
+      <div class="error" v-if="!validationStatus.name.valid">
+        {{ validationStatus.name.message }}
+      </div>
+      <label for="name">Name</label>
+      <input type="text" id="name" v-model="patient.name">
+    </div>
+
+    <div class="field">
+      <div class="error" v-if="!validationStatus.weight.valid">
+        {{ validationStatus.weight.message }}
+      </div>
+      <label for="weight">Weight</label>
+      <input type="text" id="weight" v-model.number="patient.weight.value">
+      <select>
+        <option value="kg">kg</option>
+        <option value="lb">lb</option>
+      </select>
+    </div>
+  </form>
 <pre>
 {{ patient }}
 </pre>
@@ -35,4 +56,16 @@ export default {
 </script>
 
 <style>
+.field > label {
+  display: inline-block;
+  width: 50px;
+  margin: 0 0 20px 0;
+}
+.field > input {
+  display: inline-block;
+  margin: 2px;
+}
+.error {
+  color: red;
+}
 </style>
