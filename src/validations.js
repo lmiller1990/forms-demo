a3133ce8778a13807a6980ecacf3d680746d3df9
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-10-20 20:57:15 +1000

    is between

--- src/validations.js
+++ src/validations.js
@@ -1,3 +1,23 @@
+export function isBetween(value, { min, max }) {
+  if (isNaN(parseInt(value))) {
+    return {
+      valid: false,
+      message: 'Must be a number'
+    }
+  }
+  
+  if (value < min || value > max) {
+    return {
+      valid: false,
+      message: `Must be between ${min} and ${max}`
+    }
+  }
+
+  return {
+    valid: true
+  }
+}
+
 export function required(value) {
   if (!value) {
     return {
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,4 +1,4 @@
-import { required } from '../../src/validations.js'
+import { required, isBetween } from '../../src/validations.js'
 
 describe('required', () => {
   it('is valid', () => {
@@ -17,3 +17,31 @@ describe('required', () => {
     expect(required(input)).toEqual(output)
   })
 })
+
+describe('isBetween', () => {
+  it('is greater than upper limit', () => {
+    const actual = isBetween(11, { min: 1, max: 10 })
+    expect(actual).toEqual({ valid: false, message: 'Must be between 1 and 10' })
+  })
+
+  it('is less than lower limit', () => {
+    const actual = isBetween(0, { min: 1, max: 10 })
+    expect(actual).toEqual({ valid: false, message: 'Must be between 1 and 10' })
+  })
+
+  it('is same as lower limit', () => {
+    const actual = isBetween(1, { min: 1, max: 10 })
+    expect(actual).toEqual({ valid: true })
+  })
+
+  it('is same as upper limit', () => {
+    const actual = isBetween(10, { min: 1, max: 10 })
+    expect(actual).toEqual({ valid: true })
+  })
+
+  it('is not valid number', () => {
+    const actual = isBetween('asdfasdf', { min: 1, max: 10 })
+    expect(actual).toEqual({ valid: false, message: 'Must be a number' })
+  })
+})
+
